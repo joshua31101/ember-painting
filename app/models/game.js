0b2bc30cfa33343f0b94c7616d4c389422c1c74b
Joshua Wang <joshuawang3110@gmail.com>
2019-05-03 09:23:26 -0400

    Define relationships in game and picture

--- app/models/game.js
+++ app/models/game.js
@@ -1,5 +1,13 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  templatePicture: DS.belongsTo('picture', { async: false }),
+  playPicture: DS.belongsTo('picture', { async: false }),
 
+  init() {
+    this._super(...arguments);
+
+    this.set('templatePicture', this.store.createRecord('picture'));
+    this.set('playPicture', this.store.createRecord('picture'));
+  },
 });
